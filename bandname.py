dc81b9d7097ffcb7f27eeb9487676a2d92d70b1e
ashleypearl-web <tamfuhashley@gmail.com>
2024-09-09 19:50:12 -0400

    created generator

---
+++ bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+name = input("Whats the name of the city you grew up in?\n")
+petname = input("Whats your pet's name?\n")
+print("Your band name could be: " + name + " " + petname)
